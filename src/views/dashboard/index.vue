c59bb379e2c64560d22c2ee4e6d624ae5e76f95d
xiaojun092666 <xiaojun19890926@qq.com>
2019-04-16 11:23:05 +0800

    xiaojun submit

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dashboard-container">
+    这是首页
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Dashboard',
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.dashboard {
+  width:100%;
+  height:100%;
+}
+</style>
